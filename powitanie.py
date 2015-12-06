7d5e7543a13eef01d84a30922c6b4b6087acf6cc
Olga Górczyńska <olgagorczynska@gmail.com>
2015-12-06 12:19:22 +0100

    My Django Girls app, first commit

---
+++ powitanie.py
@@ -0,0 +1,10 @@
+
+def czesc(imie):
+	print("Cześć " + imie + " !")
+grupa = ["Zbyszek", "Asia", "Olga", "Aga"]
+for imie in grupa:
+	czesc(imie)
+	print("Następna osoba")
+
+for i in range(1,10):
+	print(i)
